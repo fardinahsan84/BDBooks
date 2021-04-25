a2e50f92e81fbd6d79264178dc7b25c9053987e8
FARDIN AHSAN IMRAN <fardinahsan84@gmail.com>
2021-04-25 14:43:57 -0600

    header, footer, css, layout

---
+++ assets/layout/footer.php
@@ -0,0 +1,9 @@
+<?php
+ echo ' <p>
+		Contact us<br>
+		samantasadia1971@gmail.com<br>
+		Developer: Samanta Sadia<br>
+		<a href="https://github.com/samantasadia">www.github.com</a><br>
+		© Copyright 2021 BDBooks Ltd. - All Rights Reserved
+  </p>';
+?>
